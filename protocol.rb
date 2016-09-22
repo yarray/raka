8455e5fcf5d7c3f42855b5b3f5df7314149b0313
yarray <08to09@gmail.com>
2016-09-22 22:52:21 +0800

    Imporve R protocol with db settings

--- protocol.rb
+++ protocol.rb
@@ -76,17 +76,26 @@ class R < LanguageProtocol
 
     sources = ([
       :io,
-    ] + libs).map { |name| "source('src/#{name}.R')" }
+    ] + @libs).map { |name| "source('#{SRC_DIR}/#{name}.R')" }
 
     extra = [
       '`|` <- `%>>%`',
+      "conn_args <- list(host='#{HOST}', user='#{USER}', dbname='#{DB}', port='#{PORT}')",
+      "args <- commandArgs(trailingOnly = T)",
+      "sql_input    <- init_sql_input(conn_args, args[1])",
+      "table_input  <- init_table_input(conn_args, args[1])",
+      "table_output <- init_table_output(conn_args, args[1])"
     ]
 
-    [libraries, sources, extra, code].join "\n"
+    after = [
+      'system("touch args[2]")'
+    ]
+
+    [libraries, sources, extra, code, after].join "\n"
   end
 
   def run_script(env, fname, task)
-    env.send :sh, "Rscript #{fname}"
+    env.send :sh, "Rscript #{fname} '#{task.scope || "public"}'"
   end
 end
 
