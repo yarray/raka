a30d55f3724d1f43b202e420e5a34d860f4500ab
yarray <08to09@gmail.com>
2022-02-03 00:29:13 +0800

    better autocompletion

--- contrib/vscode/raka/out/extension.js
+++ contrib/vscode/raka/out/extension.js
@@ -5,7 +5,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.activate = void 0;
 const vscode = require("vscode");
-const stemDoc = `The stem of the output.
+const outputDoc = 'The output file';
+const inputDoc = `The input file, inferred from the chaining target.
+
+**Example:**
+
+the input of out/count__num__raw.csv may be out/num__raw.csv
+`;
+const inputStemDoc = `The stem of the input
+
+**Example:**
+
+the stem of out/num__raw.csv is num__raw`;
+const outputStemDoc = `The stem of the output.
 
 **Example:**
 
@@ -21,6 +33,42 @@ For csv.name["(\S+)_(\d+)"].stat with an output "out/stat__exp_12.csv":
 - *rask.captures.name* is "exp_12"
 - *rask.captures.name0* is "exp"
 - and *rask.captures.name1* is "12"`;
+const depsDoc = `The dependencies of task
+
+Dependencies are declared at the right hand of the raka rule, not including the input`;
+const funcDoc = `Last token of the chaining target
+
+**Example:**
+
+the func of out/count__num__raw.csv is "count"`;
+const scopeDoc = `Task scope, i.e. the common directory of output and dependencies
+
+**Example:**
+
+- Given a rule "csv.test = [csv.raw] ..." with output "out/2021/test.csv", the scope is out/2021
+- Given a rule "csv('group0').test = [csv.raw] ..." with output "out/2021/group0/test.csv", the scope is out/2021
+`;
+const targetScopeDoc = `Target scope, i.e. the directory specified in target
+
+**Example:**
+
+Given a rule "csv('2021/group0').test = [csv.raw] ...", the target scope is 2021/group0
+`;
+const ruleScopesDoc = `Scopes as defined at rule level, i.e. by dsl.scopes
+
+**Example:**
+
+Given a rule:
+\`\`\`ruby
+dsl.scope :group0, :group1  do
+	dsl.scope :sub0, :sub1 do
+		csv.test = ...
+	end
+end
+\`\`\`
+and output "out/group0/sub1/test.csv", the rule scopes are [:sub1, :group0]
+`;
+const extDoc = "Extension of the output file";
 function activate(context) {
     const raskProvider = vscode.languages.registerCompletionItemProvider('ruby', {
         provideCompletionItems(document, position) {
@@ -32,40 +80,23 @@ function activate(context) {
             let methods = [];
             if (linePrefix.endsWith('rask.')) {
                 methods = [
-                    ['name', 'string', 'The output file, alias to output'],
-                    ['output', 'string', 'The output file'],
-                    ['stem', 'string', stemDoc + '\nalias to output_stem'],
-                    ['output_stem', 'string', stemDoc],
+                    ['name', 'string', outputDoc + '\nalias to output'],
+                    ['output', 'string', outputDoc],
                     [
-                        'output_dir',
+                        'stem',
                         'string',
-                        'The directory of the output file',
+                        outputStemDoc + '\nalias to output_stem',
                     ],
-                    [
-                        'input',
-                        'string',
-                        'The input file, inferred from the chaining target.\n\n**Example:**\n\nthe input of out/count__num__raw.csv may be out/num__raw.csv',
-                    ],
-                    [
-                        'input_stem',
-                        'string',
-                        'The stem of the input\n\n**Example:**\n\nthe stem of out/num__raw.csv is num__raw',
-                    ],
-                    [
-                        'deps',
-                        '[string]',
-                        'The dependencies of task\n\nDependencies are declared at the right hand of the raka rule, not including the input',
-                    ],
+                    ['output_stem', 'string', outputStemDoc],
+                    ['input', 'string', inputDoc],
+                    ['input_stem', 'string', inputStemDoc],
+                    ['deps', '[string]', depsDoc],
                     ['captures', 'object', capturesDoc],
-                    [
-                        'func',
-                        'string',
-                        'Last token of the chaining target\n\n**Example:**\n\nthe func of out/count__num__raw.csv is "count"',
-                    ],
-                    ['scope', 'string', '#'],
-                    ['target_scope', 'string', '#'],
-                    ['rule_scopes', '[string]', '#'],
-                    ['ext', 'string', '#'],
+                    ['func', 'string', funcDoc],
+                    ['scope', 'string', scopeDoc],
+                    ['target_scope', 'string', targetScopeDoc],
+                    ['rule_scopes', '[string]', ruleScopesDoc],
+                    ['ext', 'string', extDoc],
                 ];
             }
             return methods.length > 0
@@ -83,43 +114,43 @@ function activate(context) {
         provideCompletionItems(document, position) {
             const vars = [
                 ['@', '$@: the output file'],
-                ['^', '$^: the dependencies, concated by ","'],
+                ['^', '$^: the dependencies, concated by ",", not including input'],
                 ['<', '$@: the input file'],
             ];
             const parenVars = [
-                ['output', 'the output file'],
-                ['input', 'the input file'],
-                ['output_stem', 'the stem of the output file'],
-                ['input_stem', 'the stem of the input file'],
-                ['deps', 'the dependencies, concated by ","'],
-                ['dep0', '$dep{i}: the i-th dependency'],
+                ['output', outputDoc],
+                ['input', inputDoc],
+                ['output_stem', outputStemDoc],
+                ['input_stem', inputStemDoc],
+                ['deps', 'The dependencies, concated by ",", not including input'],
+                ['dep0', 'The i-th dependency, not including input'],
                 [
                     'scope',
-                    'the scope of the task, i.e. the common directory for both output and dependencies',
+                    'The scope of the task, i.e. the common directory for both output and dependencies',
                 ],
                 [
                     'target_scope',
-                    'the inline scope specified by the rule target',
+                    targetScopeDoc,
                 ],
                 [
                     'target_scope0',
-                    'target_scope{i}: the i-th capture of the scope pattern specified by the rule target',
+                    'The i-th capture of the scope pattern specified by the rule target',
                 ],
                 [
                     'rule_scope0',
-                    'rule_scope{i}: the i-th rule scope, specify by nested dsl.scopes',
+                    'The i-th rule scope, specify by nested dsl.scopes\n\n' + ruleScopesDoc,
                 ],
             ];
             return vars
                 .map((v) => {
                 let item = new vscode.CompletionItem(v[0], vscode.CompletionItemKind.Variable);
-                item.detail = v[1];
+                item.documentation = new vscode.MarkdownString(v[1]);
                 return item;
             })
                 .concat(parenVars.map((v) => {
                 let item = new vscode.CompletionItem(v[0], vscode.CompletionItemKind.Variable);
                 item.insertText = `(${v[0]})`;
-                item.detail = v[1];
+                item.documentation = new vscode.MarkdownString(v[1]);
                 return item;
             }));
         },
